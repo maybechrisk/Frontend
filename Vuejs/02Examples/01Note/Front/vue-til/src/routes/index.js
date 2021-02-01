09d9ae4c9180ab80080adc63d4914346d28501c8
chrisk <maybechrisk@gmail.com>
2021-02-01 18:02:10 +0900

    vue example-note) history mode (#을 뺌)

--- Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter); // Vue.use : 플러그인을 실행하기 위해서 초기화하는 코드
 export default new VueRouter({
+  mode: 'history', // url에 #을 제거함, #을 이용해서 이 페이지가 새로운 페이지라는 것을 알지 못함.
+  // #을 뺴면 새로운 페이지로 인식
+  // 실제 배포할 때는 서버에서 url에 대한 우회, 필터링 기능을 넣어주어야한다.
   routes: [
     {
       path: '/',
