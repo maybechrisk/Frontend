7525347066c87972b9bc3ba78336c0f20b6d80da
chrisk <maybechrisk@gmail.com>
2021-02-01 17:48:46 +0900

    vue example-note)라우팅 동작확인

--- Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import LoginPage from '@/views/LoginPage'
-import SignupPage from '@/views/SignupPage'
+import LoginPage from '@/views/LoginPage';
+import SignupPage from '@/views/SignupPage';
 
 Vue.use(VueRouter); // Vue.use : 플러그인을 실행하기 위해서 초기화하는 코드
 export default new VueRouter({
   routes: [
     {
-      path: 'login',
+      path: '/login',
       component: LoginPage,
     },
     {
-      path: 'signup',
+      path: '/signup',
       component: SignupPage,
     },
   ],
