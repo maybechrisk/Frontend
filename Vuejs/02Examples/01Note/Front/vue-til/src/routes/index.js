7ec8f41826c94701a2cd4aa4b460337c1db958ca
chrisk <maybechrisk@gmail.com>
2021-02-01 17:26:18 +0900

    vue example-note) 로그인,회원가입 컴포넌트 연결

--- Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
@@ -1,5 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage'
+import SignupPage from '@/views/SignupPage'
 
 Vue.use(VueRouter); // Vue.use : 플러그인을 실행하기 위해서 초기화하는 코드
-export default new VueRouter(); // default 파일에서 하나의 변수가 변하게 됨
+export default new VueRouter({
+  routes: [
+    {
+      path: 'login',
+      component: LoginPage,
+    },
+    {
+      path: 'signup',
+      component: SignupPage,
+    },
+  ],
+}); // default 파일에서 하나의 변수가 변하게 됨
