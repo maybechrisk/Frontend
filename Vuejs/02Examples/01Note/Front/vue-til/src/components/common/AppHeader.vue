ea0fd6df310cb2115000fd67cd704440cd6dc179
chrisk <maybechrisk@gmail.com>
2021-02-01 15:09:55 +0900

    @로 절대경로 접근방법(vscode)

---
+++ Vuejs/02Examples/01Note/Front/vue-til/src/components/common/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    header
+  </div>
+</template>
+
+<script>
+// import Demo from '../../demo/basic/Demo' // 너무 번거로움
+import Demo from '@/demo/basic/Demo';  //로 간단하게 @로 하는 방법
+
+export default {};
+</script>
+
+<style></style>
