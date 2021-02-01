4a241ab6e82459a8e02d6efdbff66dc1c38a4167
chrisk <maybechrisk@gmail.com>
2021-02-01 14:17:57 +0900

    eslint, prettier 적용

--- Vuejs/02Examples/01Note/Front/vue-til/.eslintrc.js
+++ Vuejs/02Examples/01Note/Front/vue-til/.eslintrc.js
@@ -8,18 +8,33 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
+    // test code
+    // "no-console": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      'error',
+      // 아래 규칙은 개인 선호에 따라 prettier문법을 적용
+      // https://prettier.io/docs/en/options.html
+      {
+        singleQuote: true, // ''
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ]
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
+  overrides: [{
+    files: [
+      "**/__tests__/*.{j,t}s?(x)",
+      "**/tests/unit/**/*.spec.{j,t}s?(x)"
+    ],
+    env: {
+      jest: true
     }
-  ]
+  }]
 };
---
+++ Vuejs/02Examples/01Note/Front/vue-til/.prettierrc.js
@@ -0,0 +1,4 @@
+// esLint가 우선시 되므로, eslint파일 안에서 설정한다
+// {
+//   "printWidth": 80
+// }
