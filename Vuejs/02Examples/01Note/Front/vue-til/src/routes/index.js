119dd60586ff81c86820084551cf2619d4efbfe4
chrisk <maybechrisk@gmail.com>
2021-02-01 17:52:54 +0900

    vue example-note) 라우터 코드 스플리팅, 다이나믹 임포트

--- Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/routes/index.js
@@ -1,18 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import LoginPage from '@/views/LoginPage';
-import SignupPage from '@/views/SignupPage';
 
 Vue.use(VueRouter); // Vue.use : 플러그인을 실행하기 위해서 초기화하는 코드
 export default new VueRouter({
   routes: [
     {
       path: '/login',
-      component: LoginPage,
+      component: () => import('@/views/LoginPage.vue'),
     },
     {
       path: '/signup',
-      component: SignupPage,
+      component: () => import('@/views/SignupPage.vue'),
     },
   ],
 }); // default 파일에서 하나의 변수가 변하게 됨
