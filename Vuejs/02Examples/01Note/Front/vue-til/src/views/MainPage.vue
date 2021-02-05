672d1aa5d467bba0c545c09c47a1fa40108b27b2
chrisk <maybechrisk@gmail.com>
2021-02-05 17:16:53 +0900

    vue example-note) 데이터 리스트 출력

--- Vuejs/02Examples/01Note/Front/vue-til/src/views/MainPage.vue
+++ Vuejs/02Examples/01Note/Front/vue-til/src/views/MainPage.vue
@@ -1,11 +1,37 @@
 <template lang="html">
   <div class="">
-    main
+    <div class="main">
+      <div class="page-header">Today I posted</div>
+      <ul>
+        <li v-for="postItem in postItems" :key="postItem._id">
+          {{ postItem.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+export default {
+  // 생성될 때 셋팅
+  created() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      postItems: [],
+    };
+  },
+  methods: {
+    // 게시물 조회
+    async fetchData() {
+      const { data } = await fetchPosts();
+      console.log(data);
+      this.postItems = data.posts;
+    },
+  },
+};
 </script>
 
 <style lang="css" scoped></style>
