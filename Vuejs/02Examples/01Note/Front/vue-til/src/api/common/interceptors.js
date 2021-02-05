07d70d6c06f3056e18c95c0ca1f99316b5a973a1
chrisk <maybechrisk@gmail.com>
2021-02-05 17:16:39 +0900

    vue example-note) 인터셉터를 이용한 HTTP 헤더 설정

--- Vuejs/02Examples/01Note/Front/vue-til/src/api/common/interceptors.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/api/common/interceptors.js
@@ -1,25 +1,24 @@
-function setInterceptors(instance) {
+import store from '@/store/index';
+
+export function setInterceptors(instance) {
+  // 인터셉터 요청
   instance.interceptors.request.use(
     function(config) {
-      // Do something before request is sent
+      // 매 api마다 토큰을 실어서 보낸다.
+      config.headers.Authorization = store.state.token;
       return config;
     },
     function(error) {
-      // Do something with request error
       return Promise.reject(error);
     },
   );
 
-  // Add a response interceptor
+  // 인터셉터 응답
   instance.interceptors.response.use(
     function(response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
       return response;
     },
     function(error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
       return Promise.reject(error);
     },
   );
--- Vuejs/02Examples/01Note/Front/vue-til/src/api/index.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/api/index.js
@@ -4,9 +4,6 @@ import { setInterceptors } from './common/interceptors';
 function createInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
-    headers: {
-      // Authorization: store.state.token,   // 이렇게 하면 이미 실행된 상태이므로 빈 값이 들어감.
-    },
   });
   // 인터셉터에 instance 셋팅
   return setInterceptors(instance);
@@ -15,16 +12,21 @@ function createInstance() {
 // 인스턴스 생성
 const instance = createInstance();
 
-// 회원가입
+// 회원가입 API
 function registerUser(userData) {
   // const url = 'http://localhost:3000/signup';
   // return axios.post(url, userData)
   return instance.post('signup', userData);
 }
 
-// 로그인
+// 로그인 API
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 노트 조회 API
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
