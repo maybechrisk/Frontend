d68d81640aa3c2515af01534bd707f636928fdee
chrisk <maybechrisk@gmail.com>
2021-02-08 16:38:09 +0900

    vue example-note) 뷰 테스트 유틸 라이브러리 사용 (@vue/test-utils)

--- Vuejs/02Examples/01Note/Front/vue-til/src/components/__test__/LoginForm.spec.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/components/__test__/LoginForm.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import LoginForm from '../LoginForm';
 
 describe('LoginForm.vue', function() {
   // 명시된 것이 하나의 테스트 단위
   test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const instance = new Vue(LoginForm).$mount(); // 컴포넌트가 마운팅 되고
-    console.log(instance);
-    expect(instance.username).toBe(''); // 인스턴스가 있는지
+    const wrapper = shallowMount(LoginForm); // 컴포넌트가 마운팅 되고
+    expect(wrapper.vm.username).toBe(''); // 인스턴스가 있는지
   });
 });
