850bf9cabb56e20d508fab34e273aaa993a5286d
chrisk <maybechrisk@gmail.com>
2021-02-08 16:45:52 +0900

    vue example-note) 유효성 검사 테스트

--- Vuejs/02Examples/01Note/Front/vue-til/src/components/__test__/LoginForm.spec.js
+++ Vuejs/02Examples/01Note/Front/vue-til/src/components/__test__/LoginForm.spec.js
@@ -1,10 +1,18 @@
 import { shallowMount } from '@vue/test-utils';
 import LoginForm from '../LoginForm';
 
-describe('LoginForm.vue', function() {
-  // 명시된 것이 하나의 테스트 단위
-  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const wrapper = shallowMount(LoginForm); // 컴포넌트가 마운팅 되고
-    expect(wrapper.vm.username).toBe(''); // 인스턴스가 있는지
+describe('LoginForm.vue', () => {
+  test('ID는 이메일 형식이어야 한다. ', () => {
+    // 테스트 내용
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test@naver.com', // 임시로 데이터를 test로 넣음
+        };
+      },
+    });
+    const idInput = wrapper.find('#username');
+    console.log('인풋박스의 값', idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid); // 값이 email이 맞는지
   });
 });
